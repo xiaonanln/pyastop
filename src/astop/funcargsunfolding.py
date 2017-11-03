6c9c7cf041aac205581b86e2fda5ab4ece5dce2f
Seis <xiaonanln@gmail.com>
2017-11-03 22:04:58 +0800

    start func code unfolding ...

--- src/astop/funcargsunfolding.py
+++ src/astop/funcargsunfolding.py
@@ -4,6 +4,8 @@ from BaseASTOptimizer import BaseASTOptimizer
 
 class FuncArgsUnfoldingASTOptimizer(BaseASTOptimizer):
 
+	RequireTypeInference = True
+
 	def optimize(self, node):
 		if not isinstance(node, ast.Call):
 			return node, False
@@ -12,4 +14,6 @@ class FuncArgsUnfoldingASTOptimizer(BaseASTOptimizer):
 
 		return node, False
 
-
+	def tryOptimizeCall(self, call):
+		assert isinstance(call, ast.Call)
+		pass
--- src/examples/test.py
+++ src/examples/test.py
@@ -1,40 +1,35 @@
-
-import os as abc
-from os import open as open2
-
-g = 1
-
-def f1():
-	global g
-	a = 1
-	def f2():
-		a = 2
-		print a
-
-	f2()
-
-	def f3():
-		a = 1
-		b = 2
-		print a, b
-		del a, b
-
-	f3()
-	print  a
-
-	def f4():
-		g
-
-	f4()
-	print g
-	g = 100
-	print g
+import time
 
 
-f1()
-
-print g
 
 
-def py2ast():
-	a, b, c = 1, 2, 3
+
+def f(a, b, c, d):
+	pass
+
+t0 = time.time()
+for i in xrange(1000000):
+	f(1, 2, 3, 4)
+
+
+print time.time() - t0; t0 = time.time()
+
+args = [1,2,3,4]
+for i in xrange(1000000):
+	f(*args)
+
+
+print time.time() - t0; t0 = time.time()
+
+for i in xrange(1000000):
+	f(1, b=2, c=3, d=4)
+
+
+print time.time() - t0; t0 = time.time()
+
+kwargs= {'b':2, 'c': 3, 'd': 4}
+for i in xrange(1000000):
+	f(1, **kwargs)
+
+print time.time() - t0; t0 = time.time()
+
